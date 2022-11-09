88e6413e31a427cf4db163e85e34665ee26751f0
Zuber Marcell <zubermarcellpetrik@gmail.com>
2022-11-09 12:56:39 +0100

    hozzaad gomb, nev regex

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+let nameRegex = /[aA-zZ, ]{1,}/;
 let artworks = [];
 document.addEventListener('DOMContentLoaded', () => {
     let title = document.getElementById('statueName');
     let year = document.getElementById('creationYear');
     let price = document.getElementById('priceInput');
     let height = document.getElementById('statueHeight');
+    document.getElementById('addStatue').addEventListener('click', () => {
+        let helyes = false;
+        if (!nameRegex.test(title.value)) {
+            document.getElementById('wrongName').textContent = 'Hibás név';
+        }
+        else {
+            document.getElementById('wrongName').textContent = '✔️';
+            helyes = true;
+        }
+        if (!helyes) {
+            return false;
+        }
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Artwork } from "./artwork";
 import { Statue } from "./Statue";
 
+let nameRegex =/[aA-zZ, ]{1,}/;
 let artworks: Array<Artwork> = [];
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -9,5 +10,20 @@ document.addEventListener('DOMContentLoaded', () => {
     let year = document.getElementById('creationYear') as HTMLInputElement;
     let price = document.getElementById('priceInput') as HTMLInputElement;
     let height = document.getElementById('statueHeight') as HTMLInputElement;
+
+    document.getElementById('addStatue')!.addEventListener('click',() => {
+        let helyes = false;
+        if (!nameRegex.test(title.value)) {
+            document.getElementById('wrongName')!.textContent='Hibás név';
+        } else {
+            document.getElementById('wrongName')!.textContent='✔️';
+            helyes=true;
+        }
+
+        if(!helyes) {
+            return false;
+        }
+
+    });
 
 })
