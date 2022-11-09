257afb48d873d9e74082e664e79f326301e054db
Zuber Marcell <zubermarcellpetrik@gmail.com>
2022-11-09 12:28:47 +0100

    classok létrehozása, html inputok felvétele

---
+++ src/Artwork.ts
@@ -0,0 +1,6 @@
+
+export interface Artwork {
+    title: string;
+    year: number;
+    price: number;
+}
---
+++ src/Statue.ts
@@ -0,0 +1,19 @@
+import { Artwork } from "./artwork";
+
+export class Statue implements Artwork{
+
+    title: string;
+    year: number;
+    price: number;
+    height: number;
+
+    constructor (title: string, year: number, price: number, height: number) {
+        
+        this.title=title;
+        this.year=year;        
+        this.price=price;
+        this.height=height;
+    }
+
+}
+
